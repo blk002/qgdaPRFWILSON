5975c3ad5f1b41f59add75c6762694d4d7082cde
Wilson Pires <wilson.pires1609@gmail.com>
2026-04-27 13:45:39 -0400

    Updates

--- assets/chevron-right-32Lp2dGB.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./useStore-CAo-t0i_.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
---
+++ assets/chevron-right-BR64WW1Z.js
@@ -0,0 +1,1 @@
+import{s as e}from"./useStore-y0e5fHF3.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
