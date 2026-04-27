89ff84a0e1f90b5113bb5444652262a8dd1bd460
Wilson Pires <wilson.pires1609@gmail.com>
2026-04-27 13:06:12 -0400

    Updates

--- assets/chevron-up-BICiI7j_.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./useStore-BYlnI_5d.js";var t=e(`chevron-down`,[[`path`,{d:`m6 9 6 6 6-6`,key:`qrunsl`}]]),n=e(`chevron-up`,[[`path`,{d:`m18 15-6-6-6 6`,key:`153udz`}]]);export{t as n,n as t};
