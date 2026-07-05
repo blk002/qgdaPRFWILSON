a5f1010f34c39c34ac7d674edb5e37edf0f29afd
Wilson Pires <wilson.pires1609@gmail.com>
2026-07-05 18:39:34 -0400

    Updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qg-prf-cache-v8';
+const CACHE_NAME = 'qg-prf-cache-v9';
 // Base derivada da localização do próprio SW (ex.: '/qgdaPRFWILSON/')
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
 const PRECACHE_ASSETS = [
