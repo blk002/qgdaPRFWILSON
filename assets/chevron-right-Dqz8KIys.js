1637d97faec3fdacb7ef3fbf71e3596f780d3d71
Wilson Pires <wilson.pires1609@gmail.com>
2026-07-16 11:52:14 -0400

    Updates

---
+++ assets/chevron-right-Dqz8KIys.js
@@ -0,0 +1,1 @@
+import{l as e}from"./useStore-Cjea1cZ5.js";var t=e(`chevron-right`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qg-prf-cache-v24';
+const CACHE_NAME = 'qg-prf-cache-v25';
 // Base derivada da localização do próprio SW (ex.: '/qgdaPRFWILSON/')
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
 const PRECACHE_ASSETS = [
