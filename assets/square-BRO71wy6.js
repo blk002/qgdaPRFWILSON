eef991cdb54444836935f8c5db360a27838e2c76
Wilson Pires <wilson.pires1609@gmail.com>
2026-07-17 11:31:54 -0400

    Updates

--- assets/square-BRO71wy6.js
+++
@@ -1,1 +0,0 @@
-import{l as e}from"./useStore-Dja9Pne1.js";var t=e(`square`,[[`rect`,{width:`18`,height:`18`,x:`3`,y:`3`,rx:`2`,key:`afitv7`}]]);export{t};
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'qg-prf-cache-v42';
+const CACHE_NAME = 'qg-prf-cache-v43';
 // Base derivada da localização do próprio SW (ex.: '/qgdaPRFWILSON/')
 const BASE = self.location.pathname.replace(/sw\.js$/, '');
 const PRECACHE_ASSETS = [
